2b39da4c6a6166ef3a46ec494fecf407f8f08b0b
y-olave <105311380+y-olave@users.noreply.github.com>
2022-12-17 14:57:48 -0300

    .

--- seccion243-backend/sistemabackend/productos/models.py
+++ seccion243-backend/sistemabackend/productos/models.py
@@ -9,3 +9,11 @@ class productos(models.Model):
     stock = models.IntegerField()
     imagen = models.ImageField(upload_to='productos')
     
+class usuarios(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    telefono = models.IntegerField()
+    direccion = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='usuarios')
